ad4665ea4e3bf2b98d64f55e1bb7f34ac60b3e31
andy.patterson <andy.patterson@genesys.com>
2018-03-23 14:00:27 -0400

    publish dist/src so that library can be imported by parts. require('simplytyped/objects')

---
+++ scripts/movePackageJson.ts
@@ -0,0 +1,11 @@
+import * as fs from 'fs';
+
+const pack = fs.readFileSync('package.json');
+const json = JSON.parse(pack.toString());
+
+json.main = 'index';
+json.types = 'index.d.ts';
+delete json.files;
+delete json.private;
+
+fs.writeFileSync('dist/src/package.json', JSON.stringify(json, undefined, 4));
