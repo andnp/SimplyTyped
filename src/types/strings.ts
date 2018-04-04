c2193a61affb5eada5478f502ab8c7d2c60244bf
andy.patterson <andy.patterson@genesys.com>
2018-04-04 10:07:12 -0400

    DeepPartial and DeepReadonly incorrectly matched functions and arrays

--- src/types/strings.ts
+++ src/types/strings.ts
@@ -1,10 +1,7 @@
 import { True, False, And } from './conditionals';
 import { IsNever } from './predicates';
 
-/**
- * From https://github.com/Microsoft/TypeScript/issues/12215#issuecomment-311923766
- */
-export type Diff<T extends string, U extends string> = ({[K in T]: K} & Record<U, never> & Record<string, never>)[T];
+export type Diff<T extends string, U extends string> = Exclude<T, U>;
 export type DropString<T extends string, U extends T> = Diff<T, U>;
 
 export type StringEqual<T extends string, U extends string> =
