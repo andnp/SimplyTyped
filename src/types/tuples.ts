c53d400c97d9e7c80c531114e20751927a2d6ffe
andy.patterson <andy.patterson@genesys.com>
2018-04-08 20:14:28 -0400

    feat(tuple): add IntersectTuple type

--- src/types/tuples.ts
+++ src/types/tuples.ts
@@ -2,3 +2,15 @@ export interface Vector<T> { readonly [x: number]: T; readonly length: number; }
 export type Length<T extends Vector<any>> = T['length'];
 
 export type UnionizeTuple<T extends Vector<any>> = T[number];
+export type IntersectTuple<T extends Vector<any>> =
+    Length<T> extends 1 ? T[0] :
+    Length<T> extends 2 ? T[0] & T[1] :
+    Length<T> extends 3 ? T[0] & T[1] & T[2] :
+    Length<T> extends 4 ? T[0] & T[1] & T[2] & T[3] :
+    Length<T> extends 5 ? T[0] & T[1] & T[2] & T[3] & T[4] :
+    Length<T> extends 6 ? T[0] & T[1] & T[2] & T[3] & T[4] & T[5] :
+    Length<T> extends 7 ? T[0] & T[1] & T[2] & T[3] & T[4] & T[5] & T[6] :
+    Length<T> extends 8 ? T[0] & T[1] & T[2] & T[3] & T[4] & T[5] & T[6] & T[7] :
+    Length<T> extends 9 ? T[0] & T[1] & T[2] & T[3] & T[4] & T[5] & T[6] & T[7] & T[8] :
+    Length<T> extends 10 ? T[0] & T[1] & T[2] & T[3] & T[4] & T[5] & T[6] & T[7] & T[8] & T[9] :
+        any;
