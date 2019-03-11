6a041945fd1fbdca8f4a65b0556b60477a12b766
andy <andnpatterson@gmail.com>
2019-03-11 13:57:32 -0600

    chore: remove internal use of ObjectKeys

--- src/impl/objects.ts
+++ src/impl/objects.ts
@@ -6,7 +6,7 @@ import { DeepReadonly, TaggedObject } from '../types/objects';
  * @param obj object to query for key `k`
  * @param k key to check existence in `obj`
  */
-export function isKeyOf<T extends object>(obj: T, k: ObjectKeys): k is keyof T {
+export function isKeyOf<T extends object>(obj: T, k: keyof any): k is keyof T {
     return k in obj;
 }
 
@@ -34,7 +34,7 @@ export function Readonly<T extends object>(obj: T): DeepReadonly<T> { return obj
  * @param key the name of the "tag" parameter
  * @returns `obj` with the inner objects tagged with parameter `key` and the key pointing to that inner object
  */
-export function taggedObject<T extends Record<ObjectKeys, object>, K extends string>(obj: T, key: K): TaggedObject<T, K> {
+export function taggedObject<T extends Record<keyof any, object>, K extends string>(obj: T, key: K): TaggedObject<T, K> {
     const keys = objectKeys(obj);
     return keys.reduce((collection: any, k) => {
         const inner: any = obj[k];
